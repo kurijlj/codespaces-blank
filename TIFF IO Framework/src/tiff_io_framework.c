f39c135bce92905c8e4e802d3f21aa97f4fb2f38
Ljubomir Kurij <ljubomir_kurij@protonmail.com>
2023-09-07 13:16:44 +0200

    [Project: TIFF IO Framework] Added error logging time stamps. Code broken when selecting read or write file.

--- TIFF IO Framework/src/tiff_io_framework.c
+++ TIFF IO Framework/src/tiff_io_framework.c
@@ -22,7 +22,7 @@
  *
  * yyyy-mm-dd Ljubomir Kurij <ljubomir_kurij@protonmail.com>
  *
- * * <CLI_FRAMEWORK>.c: created.
+ * * <TIFF_IO_FRAMEWORK>.c: created.
  *
  * ========================================================================== */
 
@@ -39,6 +39,9 @@
  * 
  * * For how to use GNU Scientific Library consult documentation and examples
  *   at <https://www.gnu.org/software/gsl/doc/html/index.html>.
+ * 
+ * * For how to use libtiff consult documentation and examples at
+ *   <http://www.libtiff.org/libtiff.html>.
  *
  * ========================================================================== */
 
@@ -52,20 +55,23 @@
 /* System headers */
 
 /* Standard Library headers */
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 /* External libraries headers */
 #include <argparse.h>
 #include <gsl/gsl_math.h>
+#include <tiffiop.h>
 
 
 /* ==========================================================================
  * Macros definitions section
  * ========================================================================== */
 
-#define APP_NAME             "cli_framework"
+#define APP_NAME             "tiff_io_framework"
 #define APP_VERSION          "0.1"
 #define APP_AUTHOR           "Ljubomir Kurij"
 #define APP_EMAIL            "ljubomir_kurij@protonmail.com"
@@ -82,6 +88,8 @@
 #endif  /* End of platform specific macro definition */
 #define APP_EPILOGUE         "\nReport bugs to <" APP_EMAIL ">."
 
+#define TIME_STAMP_LEN       20
+
 
 /* ==========================================================================
  * Global variables section
@@ -91,20 +99,24 @@ static const char *const kUsages[] = {
     APP_USAGE_A,
     NULL,
 };
+static TIFFErrorHandler old_error_handler = NULL;
+
+#define DEFAULT_MAX_MALLOC (256 * 1024 * 1024)
+/* malloc size limit (in bytes)
+ * disabled when set to 0 */
+static tmsize_t maxMalloc = DEFAULT_MAX_MALLOC;
 
 
 /* ==========================================================================
  * Utility function declarations
  * ========================================================================== */
 
-int short_usage(
-        struct argparse *self,
-        const struct argparse_option *option
-        );
-int version_info(
-        struct argparse *self,
-        const struct argparse_option *option
-        );
+static int short_usage(struct argparse *self,
+        const struct argparse_option *option);
+static int version_info(struct argparse *self,
+        const struct argparse_option *option);
+static void custom_error_handler(const char *module,
+        const char *fmt, va_list ap);
 
 
 /* ==========================================================================
@@ -115,37 +127,58 @@ int main(int argc, char **argv) {
 
     int usage = 0;
     int version = 0;
+    char *in_file = NULL;
+    char *out_file = NULL;
 
     /* Define command line options */
     struct argparse_option options[] = {
-        OPT_GROUP("general options"),
         OPT_HELP(),
+        OPT_GROUP("general options"),
         OPT_BOOLEAN(
-                '\0',
-                "usage",
-                &usage,
-                "give a short usage message",
-                &short_usage,
-                0,
-                0
-                ),
+                    '\0',
+                    "usage",
+                    &usage,
+                    "give a short usage message",
+                    &short_usage,
+                    0,
+                    0
+                   ),
         OPT_BOOLEAN(
-                'V',
-                "version",
-                &version,
-                "print program version",
-                &version_info,
-                0,
-                0
-                ),
+                    'V',
+                    "version",
+                    &version,
+                    "print program version",
+                    &version_info,
+                    0,
+                    0
+                   ),
+        OPT_GROUP("TIFF IO options"),
+        OPT_STRING(
+                   'r',
+                   "read-from-file",
+                   in_file,
+                   "TIFF file to read from",
+                   NULL,
+                   0,
+                   0
+                  ),
+        OPT_STRING(
+                   'w',
+                   "write-to-file",
+                   out_file,
+                   "TIFF file to write to",
+                   NULL,
+                   0,
+                   0
+                  ),
         OPT_END(),
     };
 
     /* Parse command line arguments */
-    struct argparse argparse;
-    argparse_init(&argparse, options, kUsages, 0);
-    argparse_describe(&argparse, APP_DESCRIPTION, APP_EPILOGUE);
-    argc = argparse_parse(&argparse, argc, argv);
+    struct argparse argument_parser;
+    argparse_init(&argument_parser, options, kUsages, 0);
+    argparse_describe(&argument_parser, APP_DESCRIPTION, APP_EPILOGUE);
+    argc = argparse_parse(&argument_parser, argc, argv);
 
     /* Check if usage or version options were given */
     if (usage != 0 || version != 0) {
@@ -156,10 +189,100 @@ int main(int argc, char **argv) {
     /* Main module code */
     int status = EXIT_SUCCESS;
 
-    if (argc == 0) {
+    /* const char* time_stamp = __DATE__ " " __TIME__; */
+    char time_stamp[TIME_STAMP_LEN] = "yyyy-mm-dd hh:mm:ss";
+    time_t t = time(NULL);
+    TIFF *in  = NULL;
+    TIFF *out = NULL;
+
+    status = EXIT_SUCCESS;
+
+    t = time(NULL);
+    strftime(
+             time_stamp,
+             TIME_STAMP_LEN,
+             "%Y-%m-%d %H:%M:%S",
+             localtime(&t)
+            );
+    fprintf(
+            stderr,
+            "%s: %s: Start of main program execution ...\n",
+            time_stamp,
+            APP_NAME
+        );
+
+    old_error_handler = TIFFSetErrorHandler(custom_error_handler);
+
+    if (argc != 0 && NULL != in_file) {
+        TIFFOpenOptions *opts = TIFFOpenOptionsAlloc();
+        if (NULL == opts) {
+            status = EXIT_FAILURE;
+
+        } else {
+            TIFFOpenOptionsSetMaxSingleMemAlloc(opts, maxMalloc);
+            in = TIFFOpenExt(in_file, "rh", opts);
+            TIFFClose(in);
+            TIFFOpenOptionsFree(opts);
+
+        }
+
+    } else if (argc != 0 && NULL != out_file) {
+        TIFFOpenOptions *opts = TIFFOpenOptionsAlloc();
+        if (NULL == opts) {
+            status = EXIT_FAILURE;
+
+        } else {
+            TIFFOpenOptionsSetMaxSingleMemAlloc(opts, maxMalloc);
+            out = TIFFOpenExt(out_file, "w", opts);
+            TIFFClose(out);
+            TIFFOpenOptionsFree(opts);
+
+        }
+
+    } else {
         /* No arguments were given */
-        printf("%s: Main module running ...\n", APP_NAME);
-        printf("%s: PI = %f\n", APP_NAME, M_PI);
+        status = EXIT_FAILURE;
+        t = time(NULL);
+        strftime(
+                 time_stamp,
+                 TIME_STAMP_LEN,
+                 "%Y-%m-%d %H:%M:%S",
+                 localtime(&t)
+                );
+        fprintf(
+                stderr,
+                "%s: %s: Invalid call to program.\n",
+                time_stamp,
+                APP_NAME
+            );
+        short_usage(&argument_parser, options);
+        /* printf("%s: %s: PI = %f\n", time_stamp, APP_NAME, M_PI); */
+
+    }
+
+    t = time(NULL);
+    strftime(
+             time_stamp,
+             TIME_STAMP_LEN,
+             "%Y-%m-%d %H:%M:%S",
+             localtime(&t)
+            );
+
+    if (EXIT_SUCCESS == status) {
+        fprintf(
+                stderr,
+                "%s: %s: Main program finshed with: Success.\n",
+                time_stamp,
+                APP_NAME
+            );
+
+    } else {
+        fprintf(
+                stderr,
+                "%s: %s: Main program finished with: Errors.\n",
+                time_stamp,
+                APP_NAME
+            );
 
     }
 
@@ -188,7 +311,8 @@ int main(int argc, char **argv) {
  * Returns: Number of characters printed
  * 
  * -------------------------------------------------------------------------- */
-int short_usage(struct argparse *self, const struct argparse_option *option) {
+static int short_usage(struct argparse *self,
+        const struct argparse_option *option) {
     #ifdef _WIN32
     return fprintf(stdout, "%s %s\n%s%s%s\n", 
         "Usage:", APP_USAGE_A,
@@ -219,7 +343,8 @@ int short_usage(struct argparse *self, const struct argparse_option *option) {
  * Returns: Number of characters printed
  * 
  * -------------------------------------------------------------------------- */
-int version_info(struct argparse *self, const struct argparse_option *option) {
+static int version_info(struct argparse *self,
+        const struct argparse_option *option) {
     return fprintf(stdout, "%s %s %s %s %s\n%s %s: %s <%s>\n%s\n%s\n",
             APP_NAME, APP_VERSION, "Copyright (c)",
             APP_COPYRIGHT_YEAR, APP_AUTHOR,
@@ -229,5 +354,14 @@ int version_info(struct argparse *self, const struct argparse_option *option) {
             "to change and redistribute it.",
             "There is NO WARRANTY, to the extent permitted by law."
             );
+
+}
+
+
+static void custom_error_handler(const char *module,
+        const char *fmt, va_list ap)
+{
+    if (old_error_handler)
+        (*old_error_handler)(module, fmt, ap);
 
 }
